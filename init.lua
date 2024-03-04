daef108736d94fda0304d6ff4f624b63665ff3e7
maikerudev <michenbox@protonmail.com>
2024-03-04 22:29:07 +0000

    Fix missing extras

--- init.lua
+++ init.lua
@@ -1,15 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h12"
+end
 
 vim.g.clipboard = {
-  name = 'win32yank-wsl',
+  name = "win32yank-wsl",
   copy = {
-    ["+"] = { 'win32yank.exe', '-i', '--crlf' },
-    ["*"] = { 'win32yank.exe', '-i', '--crlf' },
+    ["+"] = { "win32yank.exe", "-i", "--crlf" },
+    ["*"] = { "win32yank.exe", "-i", "--crlf" },
   },
   paste = {
-    ["+"] = { 'win32yank.exe', '-o', '--lf' },
-    ["*"] = { 'win32yank.exe', '-o', '--lf' },
+    ["+"] = { "win32yank.exe", "-o", "--lf" },
+    ["*"] = { "win32yank.exe", "-o", "--lf" },
   },
   cache_enabled = false,
 }
