ee6fbdaef6a08bf2749db5050acdd94979548e65
maikerudev <michenbox@protonmail.com>
2024-03-05 16:41:11 +0000

    Fix indents

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.cmd("autocmd FileType java setlocal shiftwidth=4")
+vim.cmd("autocmd FileType java setlocal tabstop=4")
+vim.cmd("autocmd FileType java setlocal softtabstop=4")
